a402f1f5d0e1ff6bbfbc60cbd36d61084ba3ce73
Imrul Kayes <kayeskayes65@gmail.com>
2025-03-18 22:52:34 +0600

    First commit

---
+++ JS PROJECTS/main.js
@@ -0,0 +1,352 @@
+// JSON data (as a JavaScript object)
+const data = {
+  meals: [
+    {
+      idMeal: "52822",
+      strMeal: "Pasta pomodoro",
+      strMealAlternate: null,
+      strCategory: "Pork",
+      strArea: "British",
+      strInstructions:
+        "Preheat the oven to 200°C/fan180°C/gas 6. Fry sausages in a non-stick pan until browned.\nDrizzle vegetable oil in a 30cm x 25cm x 6cm deep roasting tray and heat in the oven for 5 minutes.\nPut the plain flour in a bowl, crack in the medium free-range eggs, then stir in the grated horseradish. Gradually beat in the semi-skimmed milk. Season.\nPut the sausages into the hot roasting tray and pour over the batter. Top with cherry tomatoes on the vine and cook for 30 minutes until puffed and golden.",
+      strMealThumb:
+        "https://www.deliciousmagazine.co.uk/wp-content/uploads/2024/09/2024D117_ITALIANSEO_POMODORO_2_X-1-768x960.jpg",
+      strTags: null,
+      strYoutube: "https://www.youtube.com/watch?v=t1dWkDrC330",
+      strIngredient1: "sausages",
+      strIngredient2: "vegetable oil",
+      strIngredient3: "plain flour",
+      strIngredient4: "eggs",
+      strIngredient5: "horseradish",
+      strIngredient6: "milk",
+      strIngredient7: "cherry tomatoes",
+      strIngredient8: "",
+      strIngredient9: "",
+      strIngredient10: "",
+      strIngredient11: "",
+      strIngredient12: "",
+      strIngredient13: "",
+      strIngredient14: "",
+      strIngredient15: "",
+      strIngredient16: "",
+      strIngredient17: "",
+      strIngredient18: "",
+      strIngredient19: "",
+      strIngredient20: "",
+      strMeasure1: "8",
+      strMeasure2: "3 tbsp",
+      strMeasure3: "100g",
+      strMeasure4: "2 medium",
+      strMeasure5: "1 tbsp grated",
+      strMeasure6: "225ml",
+      strMeasure7: "200g",
+      strMeasure8: "",
+      strMeasure9: "",
+      strMeasure10: "",
+      strMeasure11: "",
+      strMeasure12: "",
+      strMeasure13: "",
+      strMeasure14: "",
+      strMeasure15: "",
+      strMeasure16: "",
+      strMeasure17: "",
+      strMeasure18: "",
+      strMeasure19: "",
+      strMeasure20: "",
+      strSource:
+        "http://www.deliciousmagazine.co.uk/recipes/georgina-fuggles-horseradish-and-vine-tomato-toad-in-the-hole/",
+      strImageSource: null,
+      strCreativeCommonsConfirmed: null,
+      dateModified: null,
+    },
+    {
+      idMeal: "52823",
+      strMeal: "Pork stifado",
+      strMealAlternate: null,
+      strCategory: "Chicken",
+      strArea: "American",
+      strInstructions: "Instructions for another meal.",
+      strMealThumb:
+        "https://www.deliciousmagazine.co.uk/wp-content/uploads/2024/02/Pork-Stifado-768x959.jpg",
+      strTags: null,
+      strYoutube: "https://www.youtube.com/watch?v=anothermeal",
+      strIngredient1: "sausages",
+      strIngredient2: "vegetable oil",
+      strIngredient3: "plain flour",
+      strIngredient4: "eggs",
+      strIngredient5: "eggs",
+      strIngredient6: "horseradish",
+      strIngredient7: "milk",
+      strIngredient8: "cherry tomatoes",
+      strIngredient9: "",
+      strIngredient10: "",
+      strIngredient11: "",
+      strIngredient12: "",
+      strIngredient13: "",
+      strIngredient14: "",
+      strIngredient15: "",
+      strIngredient16: "",
+      strIngredient17: "",
+      strIngredient18: "",
+      strIngredient19: "",
+      strIngredient20: "",
+      strMeasure1: "8",
+      strMeasure2: "3 tbsp",
+      strMeasure3: "50g",
+      strMeasure4: "	100g",
+      strMeasure5: "2 medium",
+      strMeasure6: "1 tbsp grated",
+      strMeasure7: "225ml",
+      strMeasure8: "200g",
+      strMeasure9: "",
+      strMeasure10: "",
+      strMeasure11: "",
+      strMeasure12: "",
+      strMeasure13: "",
+      strMeasure14: "",
+      strMeasure15: "",
+      strMeasure16: "",
+      strMeasure17: "",
+      strMeasure18: "",
+      strMeasure19: "",
+      strMeasure20: "",
+      strSource: "http://www.deliciousmagazine.co.uk/recipes/another-meal/",
+      strImageSource: null,
+      strCreativeCommonsConfirmed: null,
+      dateModified: null,
+    },
+    {
+      idMeal: "52823",
+      strMeal: "Gamberi alla busara",
+      strMealAlternate: null,
+      strCategory: "Chicken",
+      strArea: "American",
+      strInstructions: "Instructions for another meal.",
+      strMealThumb:
+        "https://www.themealdb.com/images/media/meals/ytuvwr1503070420.jpg",
+      strTags: null,
+      strYoutube: "https://www.youtube.com/watch?v=anothermeal",
+      strIngredient1: "chicken",
+      strIngredient2: "oil",
+      strIngredient3: "flour",
+      strIngredient4: "eggs",
+      strIngredient5: "spices",
+      strIngredient6: "milk",
+      strIngredient7: "tomatoes",
+      strIngredient8: "",
+      strIngredient9: "",
+      strIngredient10: "",
+      strIngredient11: "",
+      strIngredient12: "",
+      strIngredient13: "",
+      strIngredient14: "",
+      strIngredient15: "",
+      strIngredient16: "",
+      strIngredient17: "",
+      strIngredient18: "",
+      strIngredient19: "",
+      strIngredient20: "",
+      strMeasure1: "2 tbsp olive oil",
+      strMeasure2: "12 large shell-on prawns",
+      strMeasure3: "4 garlic cloves, finely sliced",
+      strMeasure4: "500g cherry tomatoes, halved",
+      strMeasure5: "1 red chilli, finely sliced",
+      strMeasure6: "125ml dry white wine",
+      strMeasure7: "Handful chopped parsley",
+      strMeasure8: "",
+      strMeasure9: "",
+      strMeasure10: "",
+      strMeasure11: "",
+      strMeasure12: "",
+      strMeasure13: "",
+      strMeasure14: "",
+      strMeasure15: "",
+      strMeasure16: "",
+      strMeasure17: "",
+      strMeasure18: "",
+      strMeasure19: "",
+      strMeasure20: "",
+      strSource:
+        "https://www.deliciousmagazine.co.uk/wp-content/uploads/2024/08/2024D057_VEGOFMONTH_PRAWNS_1-768x960.jpg",
+      strImageSource: null,
+      strCreativeCommonsConfirmed: null,
+      dateModified: null,
+    },
+    {
+      idMeal: "52823",
+      strMeal: "Toasted pasta with smoky tomato sauce",
+      strMealAlternate: null,
+      strCategory: "Chicken",
+      strArea: "American",
+      strInstructions: "Instructions for another meal.",
+      strMealThumb:
+        "https://www.deliciousmagazine.co.uk/wp-content/uploads/2025/02/2025D007_BABC_PASTA_ROASTED__006-768x959.jpg",
+      strTags: null,
+      strYoutube: "https://www.youtube.com/watch?v=anothermeal",
+      strIngredient1: "1 large green courgette",
+      strIngredient2: "1 large yellow courgette",
+      strIngredient3: "2 medium slim aubergines",
+      strIngredient4: "3-4 large vine tomatoes",
+      strIngredient5: "3 tbsp fresh thyme leaves",
+      strIngredient6: "½ x 25g bunch basil, leaves picked",
+      strIngredient7: "",
+      strIngredient8: "",
+      strIngredient9: "",
+      strIngredient10: "",
+      strIngredient11: "",
+      strIngredient12: "",
+      strIngredient13: "",
+      strIngredient14: "",
+      strIngredient15: "",
+      strIngredient16: "",
+      strIngredient17: "",
+      strIngredient18: "",
+      strIngredient19: "",
+      strIngredient20: "",
+      strMeasure1: "2 tbsp olive oil, plus extra to brush",
+      strMeasure2: "1 onion, finely chopped",
+      strMeasure3: "5 garlic cloves, finely sliced",
+      strMeasure4: "400g tin chopped tomatoes",
+      strMeasure5: "50g unsalted butter",
+      strMeasure6: "250g mixed tomatoes, slicedl",
+      strMeasure7: "2 tsp herbes de provence",
+      strMeasure8: "150g roasted red peppers from a jar, finely chopped",
+      strMeasure9: "",
+      strMeasure10: "",
+      strMeasure11: "",
+      strMeasure12: "",
+      strMeasure13: "",
+      strMeasure14: "",
+      strMeasure15: "",
+      strMeasure16: "",
+      strMeasure17: "",
+      strMeasure18: "",
+      strMeasure19: "",
+      strMeasure20: "",
+      strSource: "http://www.deliciousmagazine.co.uk/recipes/another-meal/",
+      strImageSource: null,
+      strCreativeCommonsConfirmed: null,
+      dateModified: null,
+    },
+  ],
+};
+
+// Function to populate the HTML with JSON data
+function populateMealData(meal) {
+  // Update the meal name
+  const mealName = document.querySelector(".about h1");
+  if (mealName) {
+    mealName.textContent = meal.strMeal;
+  }
+
+  // Update the origin and category
+  const originCategory = document.querySelector(".about h4");
+  if (originCategory) {
+    originCategory.innerHTML = `
+      <span>Origin: ${meal.strArea}</span> &nbsp;•&nbsp;
+      <span>Category: ${meal.strCategory}</span>
+    `;
+  }
+
+  // Update the YouTube link
+  const youtubeLink = document.querySelector(".about .youtube");
+  if (youtubeLink) {
+    youtubeLink.href = meal.strYoutube;
+  }
+
+  // Update the meal image
+  const mealImage = document.querySelector(".about img");
+  if (mealImage) {
+    mealImage.src = meal.strMealThumb;
+    mealImage.alt = meal.strMeal;
+  }
+
+  // Update the ingredients table
+  const table = document.querySelector(".ingredient table");
+  if (table) {
+    table.innerHTML = `
+      <tr>
+        <th>Ingredient</th>
+        <th>Quantity</th>
+      </tr>
+    `;
+
+    for (let i = 1; i <= 20; i++) {
+      const ingredient = meal[`strIngredient${i}`];
+      const measure = meal[`strMeasure${i}`];
+      if (ingredient && measure) {
+        table.innerHTML += `
+          <tr class="table-row">
+            <td>${ingredient}</td>
+            <td>${measure}</td>
+          </tr>
+        `;
+      }
+    }
+  }
+
+  // Update the instructions
+  const instructions = document.querySelector(".instruction");
+  if (instructions) {
+    instructions.textContent = meal.strInstructions;
+  }
+
+  // Update the recipe source link
+  const sourceLink = document.querySelector(".source");
+  if (sourceLink) {
+    sourceLink.href = meal.strSource;
+    sourceLink.textContent = "Recipe Source";
+  }
+}
+
+// Function to get a random meal
+function getRandomMeal() {
+  const randomIndex = Math.floor(Math.random() * data.meals.length);
+  return data.meals[randomIndex];
+}
+
+// Function to search for a meal by name
+function searchMeal(query) {
+  return data.meals.find((meal) =>
+    meal.strMeal.toLowerCase().includes(query.toLowerCase())
+  );
+}
+
+// Add interactivity for search and random meal buttons
+document.addEventListener("DOMContentLoaded", () => {
+  // Search button functionality
+  const searchButton = document.querySelector(".search .btn");
+  const searchInput = document.querySelector(".search input");
+
+  searchButton.addEventListener("click", () => {
+    const query = searchInput.value.trim();
+    if (query) {
+      const meal = searchMeal(query);
+      if (meal) {
+        populateMealData(meal);
+      } else {
+        alert(`No meal found for: ${query}`);
+      }
+    } else {
+      alert("Please enter a recipe to search for.");
+    }
+  });
+
+  // Random meal button functionality
+  const randomButton = document.querySelector(".random-btn");
+
+  randomButton.addEventListener("click", () => {
+    const meal = getRandomMeal();
+    populateMealData(meal);
+  });
+
+  // Save button functionality
+  const saveButton = document.querySelector(".svbtn button");
+
+  saveButton.addEventListener("click", () => {
+    alert("Recipe saved!");
+  });
+
+  // Initially populate with a random meal
+  populateMealData(getRandomMeal());
+});
